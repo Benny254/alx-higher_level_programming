fe6c198a472280dcc49e2d35489bf7d1d54c754e
Benny254 <asshimbenny01@mail.com>
2023-10-30 13:06:08 -0700

    update

---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+# Define the word
+word = "Holberton"
+
+# Extract the first 3 letters
+word_first_3 = word[:3]
+
+# Extract the last 2 letters
+word_last_2 = word[-2:]
+
+# Extract the middle portion
+middle_word = word[1:-1]
+
+# Print the results
+print(f"First 3 letters: {word_first_3}")
+print(f"Last 2 letters: {word_last_2}")
+print(f"Middle word: {middle_word}")
+
