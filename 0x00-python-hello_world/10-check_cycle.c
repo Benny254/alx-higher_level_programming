2264d804ecbeedf811f8a60eb1575b911470ae35
Benny254 <asshimbenny01@mail.com>
2023-10-30 14:35:50 -0700

    update

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -14,28 +14,30 @@ int check_cycle(listint_t *list)
 
 	p2 = list;
 	prev = list;
-	for (; list && p2 && p2->next; list = list->next, p2 = p2->next->next)
+	while (list && p2 && p2->next)
 	{
+		list = list->next;
+		p2 = p2->next->next;
+
 		if (list == p2)
 		{
-		list = prev;
-		prev = p2;
-		for (;;)
-		{
-			p2 = prev;
-			while (p2->next != list && p2->next != prev)
-		{
-			p2 = p2->next;
-		}
-		if (p2->next == list)
-			break;
+			list = prev;
+			prev =  p2;
+			while (1)
+			{
+				p2 = prev;
+				while (p2->next != list && p2->next != prev)
+				{
+					p2 = p2->next;
+				}
+				if (p2->next == list)
+					break;
 
-			list = list->next;
-		}
+				list = list->next;
+			}
 			return (1);
 		}
 	}
 
 	return (0);
 }
-
