eb9cdb8471cd5bf4dae5822fa812bf4cc685d5bf
Benny254 <asshimbenny01@mail.com>
2023-10-31 15:00:09 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-i = 0
-while i < 26:
-    char = chr(ord('a') + i)
-    print("{}".format(char), end="")
-    i += 1
+for j in "abcdefghijklmnopqrstuvwxyz":
+    print("{}".format(j), end="")
