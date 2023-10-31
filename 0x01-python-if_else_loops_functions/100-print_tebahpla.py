1f92c368e302d0d371d63df2b8e2add9d1dc1773
Benny254 <asshimbenny01@mail.com>
2023-10-31 15:18:32 -0700

    update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-n = 90
-while n >= 65:
-    if n % 2 == 0:
-        print("{}".format(chr(n + 32)), end="")
-    else:
-        print(chr(n), end="")
-    n -= 1
+print("".join(["{}".format(chr(n + 32)) if n % 2 == 0 else chr(n)
+               for n in range(90, 64, -1)]), end="")
