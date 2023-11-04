65c5f61687279e111f99d2aa12c8f13032df8db6
Benny254 <asshimbenny01@mail.com>
2023-11-04 04:25:02 -0700

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-def unique_add_tuple(tuple_a=(), tuple_b=()):
+def add_tuple(tuple_a=(), tuple_b=()):
     len_a = len(tuple_a)
     len_b = len(tuple_b)
 
     if len_a == 0:
-        val_a1, val_a2 = 0, 0
+        a3 = 0
+        a4 = 0
     elif len_a == 1:
-        val_a1, val_a2 = tuple_a[0], 0
+        a3 = tuple_a[0]
+        a4 = 0
     else:
-        val_a1, val_a2 = tuple_a[0], tuple_a[1]
+        a3 = tuple_a[0]
+        a4 = tuple_a[1]
 
     if len_b == 0:
-        val_b1, val_b2 = 0, 0
+        b3 = 0
+        b4 = 0
     elif len_b == 1:
-        val_b1, val_b2 = tuple_b[0], 0
+        b3 = tuple_b[0]
+        b4 = 0
     else:
-        val_b1, val_b2 = tuple_b[0], tuple_b[1]
+        b3 = tuple_b[0]
+        b4 = tuple_b[1]
 
-    result_tuple = (val_a1 + val_b1, val_a2 + val_b2)
+    new_tuple = (a3 + b3, a4 + b4)
 
-    return result_tuple
-
+    return (new_tuple)
