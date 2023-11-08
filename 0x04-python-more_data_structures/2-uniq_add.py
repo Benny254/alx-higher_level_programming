589d41b480a77bc9fc7579aedec292ddc37974f3
Benny254 <asshimbenny01@mail.com>
2023-11-08 02:53:57 -0800

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+# 2-uniq_add.py
+
 def uniq_add(my_list=[]):
-    return(sum(set(my_list)))
+    """Add all unique integers in a list."""
+    result = 0
+    for t in set(my_list):
+        result += t
+    return (result)
    
