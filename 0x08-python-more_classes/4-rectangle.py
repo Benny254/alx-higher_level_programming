d42b4c91df28d4b3463c7fd351e126b102ce75f9
Benny254 <asshimbenny01@mail.com>
2023-11-27 03:06:35 -0800

    update

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -41,18 +41,6 @@ class Rectangle:
         self.__height = value
 
     def __check_valid_width(self, width):
-        """
-
-        Checks if the width is a valid integer
-
-        Args:
-            width (int): The width of the Rectangle.
-
-        Raises:
-            TypeError: If `width` type is not `int`.
-            ValueError: If `width` is less than `0`.
-
-        """
 
         if self.__check_int_value(width) is False:
             raise TypeError('width must be an integer')
@@ -61,18 +49,6 @@ class Rectangle:
             raise ValueError('width must be >= 0')
 
     def __check_valid_height(self, height):
-        """
-
-        Checks if the height is a valid integer
-
-        Args:
-            height (int): The height of the Rectangle.
-
-        Raises:
-            TypeError: If `height` type is not `int`.
-            ValueError: If `height` is less than `0`.
-
-        """
 
         if self.__check_int_value(height) is False:
             raise TypeError('height must be an integer')
@@ -81,17 +57,6 @@ class Rectangle:
             raise ValueError('height must be >= 0')
 
     def __check_int_value(self, value):
-        """
-
-        Checks if the value is an integer
-
-        Args:
-            value (int): The number to verify
-
-        Returns:
-            int: If is a int `True`, `False` otherwise.
-
-        """
 
         if type(value) is int:
             return True
@@ -99,18 +64,6 @@ class Rectangle:
         return False
 
     def __check_positive_value(self, value):
-        """
-
-        Checks if the value is a positive integer
-
-        Args:
-            value (int): The number to verify
-
-        Returns:
-            int: `True` If value is greater than
-            or equal to 0, `False` otherwise.
-
-        """
 
         if value >= 0:
             return True
