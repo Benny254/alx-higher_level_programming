5398b7a4f9a74f02af6e1c70e5b9f9012a16a6f8
Benny254 <asshimbenny01@mail.com>
2023-12-05 11:21:18 -0800

    update

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
+"""append_after module"""
+
+
 def append_after(filename="", search_string="", new_string=""):
-    """class body.
-    """
     text = ""
     with open(filename) as r:
-        line = r.readline()
-        while line:
+        for line in r:
             text += line
             if search_string in line:
                 text += new_string
-            line = r.readline()
-            
     with open(filename, "w") as w:
         w.write(text)
