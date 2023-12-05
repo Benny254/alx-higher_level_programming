4ccf2ef89d8a78895af5ee27c8c1a4549dac8856
Benny254 <asshimbenny01@mail.com>
2023-12-05 10:35:47 -0800

    update

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,16 +1,17 @@
+#!/usr/bin/python3
 class Student:
     # ... (existing code)
 
     def reload_from_json(self, json):
         # Get an iterator for the items in the dictionary
         iterator = iter(json.items())
-        
+
         # Use a while loop to iterate over the items
         while True:
             try:
                 # Get the next key-value pair
                 k, v = next(iterator)
-                
+
                 # Set the attribute on the object
                 setattr(self, k, v)
             except StopIteration:
