df01028fb46f96f42beab62ad687b5f4fe5603d3
Benny254 <asshimbenny01@mail.com>
2024-01-17 12:32:58 -0800

    update

--- 0x0D-SQL_introduction/100-not_my_genres.sql
+++ 0x0D-SQL_introduction/100-not_my_genres.sql
@@ -1,21 +1,20 @@
--- Lists all genres not linked to
--- the show `Dexter`.
+-- Lists all genres of the database hbtn_0d_tvshows
+-- not linked to the show Dexter.
+-- Records are sorted by ascending genre name.
+SELECT DISTINCT `name`
+  FROM `tv_genres` AS g
+       INNER JOIN `tv_show_genres` AS s
+       ON g.`id` = s.`genre_id`
 
-SELECT DISTINCT tv_genres.name
-  FROM tv_shows
-       JOIN tv_show_genres
-       ON tv_shows.id = tv_show_genres.show_id
+       INNER JOIN `tv_shows` AS t
+       ON s.`show_id` = t.`id`
+       WHERE g.`name` NOT IN
+             (SELECT `name`
+                FROM `tv_genres` AS g
+	             INNER JOIN `tv_show_genres` AS s
+		     ON g.`id` = s.`genre_id`
 
-       JOIN tv_genres
-       ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name NOT IN (
-    SELECT tv_genres.name
-    FROM tv_shows
-         JOIN tv_show_genres
-         ON tv_shows.id = tv_show_genres.show_id
-
-         JOIN tv_genres
-         ON tv_show_genres.genre_id = tv_genres.id
-    WHERE tv_shows.title = 'Dexter'
-)
-ORDER BY tv_genres.name ASC;
+		     INNER JOIN `tv_shows` AS t
+		     ON s.`show_id` = t.`id`
+		     WHERE t.`title` = "Dexter")
+ ORDER BY g.`name`;
