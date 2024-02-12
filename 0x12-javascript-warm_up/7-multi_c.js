2719a051049935a07305f1a6a02a30d3294db880
Benny254 <asshimbenny01@gmail.com>
2024-02-12 14:52:53 -0800

    update

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
-if (process.argv[2] === undefined || isNaN(process.argv[2])) {
-  console.log('Missing number of occurrences');
+if (isNaN(parseInt(process.argv[2], 10))) {
+  console.log('Missing number of occurences');
 } else {
-  const x = Number(process.argv[2]);
-  let i = 0;
-  while (i < x) {
+  let i;
+  for (i = 0; i < process.argv[2]; i++) {
     console.log('C is fun');
-    i++;
   }
 }
