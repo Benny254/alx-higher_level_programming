945cd53a2da1241e33809874790ac1dad75de47e
Benny254 <asshimbenny01@gmail.com>
2024-02-12 14:46:36 -0800

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-for (const line of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(line);
+const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let j;
+for (j = 0; j < strings.length; j++) {
+  console.log(strings[j]);
 }
