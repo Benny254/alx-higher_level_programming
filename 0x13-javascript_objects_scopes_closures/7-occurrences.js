cac2033447781d4bc375b4bf2073a4ecad7bfb05
Benny254 <asshimbenny01@gmail.com>
2024-02-13 02:18:01 -0800

    update

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -4,7 +4,7 @@ module.exports = class Square extends require('./5-square.js') {
     if (c === undefined) {
       this.print();
     } else {
-      for (let w = 0; w < this.height; w++) console.log(c.repeat(this.width));
+      for (let j = 0; j < this.height; j++) console.log(c.repeat(this.width));
     }
   }
 };
