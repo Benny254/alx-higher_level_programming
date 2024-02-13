030c9b2576e90a7f4b1b6f84d39926b2a482bb4e
Benny254 <asshimbenny01@gmail.com>
2024-02-13 05:02:19 -0800

    update

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data.js').list;
 console.log(list);
-const map1 = list.map((x, i) => { return x * i; });
+const map1 = list.map((x, i) => { return x * (i + 1); }); // Adjusted to multiply by (i + 1)
 console.log(map1);
