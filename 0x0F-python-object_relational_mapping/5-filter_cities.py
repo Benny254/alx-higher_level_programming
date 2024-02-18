a8ebc2e69b55a884ca9534a571ac5a508e12480e
Benny254 <asshimbenny01@gmail.com>
2024-02-18 12:21:34 -0800

    update

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
-import sys
-import MySQLdb
+""" This module connects to a MySQL server and lists all
+states that match a specified name in argv. """
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states INNER JOIN " +
+                   "cities ON states.id = cities.state_id " +
+                   "WHERE states.name LIKE %s" +
+                   " ORDER BY cities.id ASC", (sys.argv[4], ))
+    results = cursor.fetchall()
+
+    for i in range(len(results)):
+        print(str(results[i][4]), end='')
+        if i != (len(results) - 1):
+            print(", ", end='')
+    print("")
+
+    db.close())
