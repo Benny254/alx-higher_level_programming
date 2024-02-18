03f246256c7e5eb8ad3da7a563c30592d3096fb5
Benny254 <asshimbenny01@gmail.com>
2024-02-18 12:11:16 -0800

    update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
-# Displays all values in the states table of the database hbtn_0e_0_usa
-# whose name matches that supplied as argument.
-# Safe from SQL injections.
-# Usage: ./3-my_safe_filter_states.py <mysql username> \
-#                                     <mysql password> \
-#                                     <database name> \
-#                                     <state name searched>
-import sys
-import MySQLdb
+""" This module connects to a MySQL server and lists all
+states that match a specified name in argv. """
+
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `states`")
-    [print(state) for state in c.fetchall() if state[1] == sys.argv[4]]
+
+    import MySQLdb
+    import sys
+
+    db = MySQLdb.connect("localhost", sys.argv[1], sys.argv[2], sys.argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states WHERE name = %s", (sys.argv[4], ))
+
+    results = cursor.fetchall()
+
+    for row in results:
+        print("(" + str(row[0]) + ", '" + str(row[1]) + "')")
+
+    db.close()
