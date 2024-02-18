2080d14ee38cdf6438d5000f8af29b570992c5b9
Benny254 <asshimbenny01@gmail.com>
2024-02-18 13:18:13 -0800

    update

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
-""" This module all objects are listed from the ORM """
-
+"""Updates the name of a State object in the database"""
 
 if __name__ == "__main__":
-    from model_state import Base, State
-    from sqlalchemy.ext.declarative import declarative_base
     import sys
+    from model_state import Base, State
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
 
+    # Create the database engine
     engine = create_engine("mysql://{}:{}@localhost/{}".
                            format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
+    # Create a configured "Session" class
     Session = sessionmaker(bind=engine)
+
+    # Create a Session
     session = Session()
 
-    state_change = session.query(State).filter_by(id='2').first()
-    state_change.name = 'New Mexico'
-    session.commit()
+    # Query the State object with id = 2 and change its name to 'New Mexico'
+    state_to_update = session.query(State).filter_by(id=2).first()
+    if state_to_update:
+        state_to_update.name = 'New Mexico'
+        session.commit()
