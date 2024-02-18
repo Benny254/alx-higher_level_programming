f3aa6a32796869e53f14abaf32f086c93ad7a193
Benny254 <asshimbenny01@gmail.com>
2024-02-18 13:29:15 -0800

    update

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-# The list all City objects from the database hbtn_0e_14_usa.
-# Usage: ./14-model_city_fetch_by_state.py <mysql username> /
-#                                          <mysql password> /
-#                                          <database name>
-import sys
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from model_state import State
-from model_city import City
+""" This module all objects are listed from the ORM """
+
 
 if __name__ == "__main__":
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    from model_city import Base, City
+    from model_state import Base, State
+    from sqlalchemy.ext.declarative import declarative_base
+    import sys
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine("mysql://{}:{}@localhost/{}".
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]))
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    for city, state in session.query(City, State) \
-                              .filter(City.state_id == State.id) \
-                              .order_by(City.id):
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    query_sc = session.query(State.name.label('state__name'),
+                             State.id.label('state__id'),
+                             City.id.label('city_id'),
+                             City.name.label('city_name')
+                             ).join(City, State.id == City.state_id)
+
+    for state_city in query_sc.order_by(City.id):
+        print(state_city.state__name + ": " + "(" + str(state_city.city_id) +
+              ") " + state_city.city_name)
