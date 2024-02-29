041e5f4cc21416fbebc3d08a576326d6f8c30afa
Benny254 <asshimbenny01@mail.com>
2024-02-29 06:27:46 -0800

    update

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
-
-# Check if URL is provided as argument
-if [ $# -ne 1 ]; then
-	echo "Usage: $0 <URL>"
-	exit 1
-fi
-
-# Send OPTIONS request to the server using curl
-response=$(curl -s -X OPTIONS -i "$1")
-
-# Extract and display allowed HTTP methods from the response
-allowed_methods=$(echo "$response" | grep -i "Allow:" | awk '{print $2}')
-echo "Allowed HTTP methods for $1 are: $allowed_methods"
+# display all HTTP methods the server will accept using curl.
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
