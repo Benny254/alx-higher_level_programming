e82a3a81e56abf58eb2ac703b7014f9cc8723464
Benny254 <asshimbenny01@mail.com>
2024-02-29 06:44:54 -0800

    update

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
-# DELETE request to the URL passed as the first argument and displays the body of the response
-curl -sX DELETE "$1"
+
+# Check if URL argument is provided
+if [ $# -ne 1 ]; then
+	echo "Usage: $0 <URL>"
+	exit 1
+fi
+
+# URL to send DELETE request
+URL=$1
+
+# Send DELETE request and display response body
+curl -X DELETE -sSL "$URL"
