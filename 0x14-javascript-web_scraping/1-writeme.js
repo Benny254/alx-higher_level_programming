9fe22bf8e03b7244802b0273d315a12f94bfcfee
Benny254 <asshimbenny01@gmail.com>
2024-03-26 14:24:34 -0700

    update

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,21 +1,10 @@
 #!/usr/bin/node
-
-import sys
-
-def write_to_file(file_path, content):
-    try:
-        with open(file_path, 'w', encoding='utf-8') as file:
-            file.write(content)
-        print("Content successfully written to the file.")
-    except Exception as e:
-        print("An error occurred while writing to the file:", e)
-
-if __name__ == "__main__":
-    if len(sys.argv) != 3:
-        print("Usage: python script.py <file_path> <content>")
-        sys.exit(1)
-    
-    file_path = sys.argv[1]
-    content = sys.argv[2]
-    
-    write_to_file(file_path, content)
+const process = require('process');
+const fs = require('fs');
+const url = String(process.argv[2]);
+const message = String(process.argv[3]);
+fs.writeFile(url, message, 'utf8', function (err, data) {
+	  if (err) {
+		      return console.log(err);
+		    }
+});
