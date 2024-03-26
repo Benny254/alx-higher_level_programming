58a2dab5fa082c37d9b3b000ab5022ca30834316
Benny254 <asshimbenny01@gmail.com>
2024-03-26 13:34:52 -0700

    update

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,20 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-
-function getStatusCode(url) {
-	    request.get(url, (error, response) => {
-		            if (error) {
-				                console.error(`Error: ${error.message}`);
-				            } else {
-						                console.log(`code: ${response.statusCode}`);
-						            }
-		        });
-}
-
-if (process.argv.length !== 3) {
-	    console.error('Usage: node script.js <URL>');
-	    process.exit(1);
-}
-
-const url = process.argv[2];
-getStatusCode(url);
+request.get(process.argv[2]).on('response', function (response) {
+	  console.log(`code: ${response.statusCode}`);
+});
